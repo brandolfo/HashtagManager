650cafe46257b01be412e027cc6e92fb4ae3f360
santiago chiabrando <santiagochiabrando@hotmail.com>
2020-11-05 20:59:24 -0300

    Agregado perfil de automap para Post y User

---
+++ Mapper/AutoMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HashtagManager.Models;
+
+namespace HashtagManager.Mapper
+{
+		public class AutoMapperProfile : Profile 
+		{
+			public AutoMapperProfile() 
+			{
+				CreateMap<Post, PostDTO>().ReverseMap();
+				CreateMap<User, UserDTO>().ReverseMap();
+			}
+		}
+}
