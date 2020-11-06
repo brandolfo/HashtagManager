8333e2b8f46b611fa6f10bf9887532cb4226c0e6
santiago chiabrando <santiagochiabrando@hotmail.com>
2020-11-05 21:04:32 -0300

    Mas migraciones

--- Migrations/20201105204743_DTOs.Designer.cs
+++ Migrations/20201105204743_DTOs.Designer.cs
@@ -4,14 +4,16 @@ using HashtagManager.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HashtagManager.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20201105204743_DTOs")]
+    partial class DTOs
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20201105210710_DTOsFinal.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HashtagManager.Migrations
+{
+    public partial class DTOsFinal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
